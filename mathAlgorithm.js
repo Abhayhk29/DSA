735a53a23d3feca9a12b498fa3ca7a34482ad872
Abhay Kumar Pandey <abhayhk29@gmail.com>
2022-08-07 17:07:37 +0530

    Search
    Linear , Binary and recursive Binary Search

--- mathAlgorithm.js
+++ mathAlgorithm.js
@@ -1,4 +1,5 @@
 // Fibonacci Sequence Series
+// O(n) Big O
 function fibnoacci(n){
     const fib = [0,1];
 
@@ -9,4 +10,92 @@ function fibnoacci(n){
     return fib;
 }
 
-console.log(fibnoacci(7));
+// big = O(n)
+function factorialNumber(n){
+    
+    let result = 1;
+    for (let i = 2; i <= n; i++){
+        result *= i;
+    }
+    return result;
+}
+
+// Big O = O(n)
+function isPrimeNumber(n){
+    if(n < 2) return false;
+
+    for (let i = 2; i < n; i++) {
+        if( n % i == 0){
+            return false;
+        }
+        
+    }
+
+    return true;
+}
+
+
+// O(n)
+function isPrimeNumber2(n){
+    if(n < 2) return false;
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if( n % i == 0){
+            return false;
+        }
+        
+    }
+
+    return true;
+}
+
+// power of 2
+//Big O = O(log(n))
+function powerOf2(n){
+    if(n < 1) return false;
+
+    while(n > 1){
+        if(n % 2 !== 0){
+            return false;
+        }
+        n = n /2;
+    }
+
+    return true;
+}
+
+// BigO : O(1)
+function isPowerOfTwoBitWise(n){
+    if(n < 1){
+        return false;
+    }
+    return (n & ( n - 1)) === 0;
+
+}
+
+
+// Recursion for Example Fibnoccie
+// O(n^2) (Big O)
+function fibnoacciRec(n){
+    if(n < 2){
+        return n;
+    }        
+
+    return fibnoacciRec(n - 1) + fibnoacciRec( n - 2);
+}
+
+// faactorial of a number
+// Big O = O(n)
+function reFact(n){
+    if(n < 2){
+        return n;
+    }
+
+    return n * reFact(n - 1);
+}
+
+
+
+ 
+
+console.log(reFact(5));
---
+++ searchAlgorithm.js
@@ -0,0 +1,54 @@
+// linear search
+// Big O = O(n)
+function linearSearch(arr,n){
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] == n){
+        return true;
+    }   
+  }  
+
+  return false;
+}
+
+// sorted array
+// Big O = O(log(n))
+function binarySearch(arr,n){
+    let start = 0;
+    let end = arr.length - 1;
+
+    while(start < end){
+        let mid = Math.floor((start + end) / 2);
+        if(arr[mid] == n){
+            return mid;
+        }
+
+        if(arr[mid] > n){
+            end = mid -1;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+function reBinarySolution(arr,n,start, end){
+   if(start > end){
+     return -1;
+   }
+   
+   let mid = Math.floor((start + end) / 2);
+
+   if(arr[mid] == n){
+     return mid;
+   }
+
+   if(arr[mid] > n){
+    reBinarySolution(arr,n, start, mid - 1);
+    } else {
+        reBinarySolution(arr,n, mid + 1, end);
+    }
+
+}
+
+console.log(binarySearch([3,5,6,7,8,9,12,22,34,45],24, 0 , [3,5,6,7,8,9,12,22,34,45].length - 1 ))
