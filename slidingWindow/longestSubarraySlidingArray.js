c49446de41ceac6912d458c3dc937ff60a887ac0
abhayhk29 <abhayhk29@gmail.com>
2025-04-04 10:53:16 +0530

    practice

--- slidingWindow/longestSubarraySlidingArray.js
+++ slidingWindow/longestSubarraySlidingArray.js
@@ -20,7 +20,7 @@ while(end < str.length){
     } else {
         obj[str[end]] = 1;
     }
-    console.log(obj);
+    // console.log(obj);
     if(Object.keys(obj).length < k){
         end++
     } else if (Object.keys(obj).length === k){
@@ -43,6 +43,50 @@ while(end < str.length){
 
 
 
-console.log(Object.values(obj))
-console.log(Object.keys(obj));
-console.log(max);
+// console.log(Object.values(obj))
+// console.log(Object.keys(obj));
+// console.log(max);
+
+
+function longestSubstring(str,k){
+    let start = 0;
+    let end = 0;
+    let max = 0;
+
+    let obj = {
+
+    }
+
+    while (end < str.length) {
+        if(obj[str[end]]){
+            obj[str[end]] += 1;
+        }else{
+            obj[str[end]] = 1;
+        }
+
+        // let keyLength = 
+        console.log(obj)
+        if(Object.keys(obj).length < k){
+            end++;
+        } else if(Object.keys(obj).length === k){
+            max = Math.max(end - start + 1, max);
+            end++;
+        } else if (Object.keys(obj).length > k){
+            while(Object.keys(obj).length > k){
+                if(obj[str[start]] > 1){
+                    obj[str[start]] -= 1;
+                }else{
+                    delete obj[str[start]];
+                }
+                start++;
+            }
+            end++;
+        }
+    }
+
+    return max;
+}
+
+
+console.log(max)
+console.log(longestSubstring(str,k))
--- slidingWindow/variableSizeSlidingWindow.js
+++ slidingWindow/variableSizeSlidingWindow.js
@@ -6,6 +6,8 @@
 // identification : arr /string 
 // condition based window size
 
+// diff  
+
 let arr = [4,1,1,1,2,3,5];
 let start = 0;
 let end = 0;
@@ -32,4 +34,53 @@ while (end < arr.length) {
     }
 }
 
+function variableSlidingWindow(arr,k){
+    let start = 0;
+    let end = 0;
+
+    let sum  = 0;
+    let max = 0;
+
+    // while (end < arr.length) {
+    //     sum += arr[end];
+    //     if(sum < k){
+    //         end++;
+    //     } else if(sum === k){
+    //         if((end -start + 1) > max){
+    //             max = end - start + 1;
+    //         }
+    //         end++;
+    //     }else if (sum > k){
+    //         while(sum > k){
+    //             sum -= arr[start];
+    //             start++;
+    //         }
+    //         end++;
+    //     }
+    // }
+
+    while (end < arr.length) {
+        sum += arr[end];
+
+        if(sum < k){
+            end++;
+        }else if(sum === k){
+            // if((end - start + 1) > max){
+            //     max = end - start + 1;
+            // }
+            max = Math.max(max, (end - start + 1))
+            end++;
+        } else if(sum > k){
+            while(sum > k){
+                sum -= arr[start];
+                start++;
+            }
+            end++
+        }
+    }
+
+    return max;
+}
+
+console.log(variableSlidingWindow(arr,k))
 console.log(max);
