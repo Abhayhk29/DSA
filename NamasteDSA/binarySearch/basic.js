666bf9955ae3fbfda4778f6ebd5b013d03f186cc
abhayhk29 <abhayhk29@gmail.com>
2025-07-15 21:25:55 +0530

    practice basic

--- NamasteDSA/binarySearch/basic.js
+++ NamasteDSA/binarySearch/basic.js
@@ -108,3 +108,205 @@ var search = function(nums, target) {
 };
 
 // 33 leetcode
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let start = 0;
+    let end = nums.length - 1;
+
+    while(start < end){
+        let mid = start + Math.floor((end - start) / 2);
+        if(nums[mid] < nums[mid + 1]){
+            start = mid + 1;
+        }else{
+            end = mid;
+        }
+    }
+
+    return start;
+};
+
+// 162 leetcode
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let s = 0;
+    let e = nums.length - 1;
+    // if(nums[s] <= nums[e]) return nums[s]
+    while(s <= e){
+        let m = s + Math.floor((e - s) / 2);
+        if(nums[s] <= nums[e]) return nums[s]
+
+        if(nums[m] < nums[m - 1]){
+            return nums[m];
+        } else if(nums[m] < nums[s]){
+            e = m - 1;
+        }else{
+            s = m + 1;
+        }
+    }
+};
+
+// 153 Find Minimum In rotated Sorted Array
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let start = 1;
+        let end = n;
+
+        while(start < end){
+            let mid = start + Math.floor((end  - start) / 2);
+            const isBad = isBadVersion(mid);
+            if(!isBad){
+                start = mid + 1;
+            }else{
+               end = mid
+            }
+        }
+        return end
+    };
+};
+
+// leetcode 278 first bad version
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    if(nums.length === 0) return [-1, -1]
+
+    let s = 0;
+    let e = nums.length - 1;
+    let ans = [-1, -1];
+    while(s < e){
+        let m = s + Math.floor((e - s) / 2);
+
+        if(nums[m] < target){
+            s = m + 1;
+        }else{
+            e = m
+        }
+    }
+
+    if(nums[s] === target){
+        ans[0] = s
+    }
+
+    s = 0;
+    e = nums.length - 1;
+
+    while(s < e){
+        let m = s + Math.ceil((e - s) / 2);
+
+        if(nums[m] > target) e = m - 1;
+        else s = m;
+    }
+
+    if(nums[e] === target){
+        ans[1] = e
+    }
+
+    return ans;
+};
+
+// leetcode 34 find first and last position of element in sorted array
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let s = 0;
+    let e = nums.length - 1;
+
+    let ans = [-1,-1];
+
+    while(s <= e){
+        let m = s + Math.floor((e - s) / 2);
+        if(nums[m] === target){
+            ans[0] = m;
+            e = m - 1;
+        }else if(nums[m]< target){
+            s = m + 1;
+        }else{
+            e = m - 1;
+        }
+    }
+    
+    s = 0;
+    e = nums.length - 1;
+     while(s <= e){
+        let m = s + Math.floor((e - s) / 2);
+        if(nums[m] === target){
+            ans[1] = m;
+            s = m + 1;
+        }else if(nums[m]< target){
+            s = m + 1;
+        }else{
+            e = m - 1;
+        }
+    }
+    return ans;
+};
+
+// leetcode 34 find first and last position of element in sorted array
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(arr) {
+    let s = 0;
+    let e = arr.length -1;
+    while(s <= e){
+        let m = s + Math.floor((e - s) / 2);
+        if(arr[m] == arr[m - 1]){
+            let leftCount = m - 1 -s;
+            if(leftCount % 2 === 1){
+                e = m - 2;
+            }else{
+                s = m  + 1;
+            }
+        } else if(arr[m] == arr[m+ 1]){
+            let lCount = m-s;
+            if(lCount % 2 === 1){
+                e = m - 1;
+            }else{
+                s = m + 2
+            }
+        }else{
+            return arr[m];
+        }
+    }
+};
+
+// leetcode 540 find first and last position of element in sorted array
---
+++ NamasteDSA/slidingWindow/basic.js
@@ -0,0 +1,108 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let mapObj = {};
+    for(let i = 0; i < nums.length; i++){
+        mapObj[nums[i]] = i
+    }
+
+    for(let j = 0; j < nums.length; j++){
+        const sumLeft = target - nums[j];
+        if(mapObj[sumLeft] && mapObj[sumLeft] != j){
+            return [j, mapObj[sumLeft]]
+        }
+    }
+};
+
+
+// two sum leetcode 1
+// s s(n)
+// t O(n)
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let right = 0;
+    let left = numbers.length - 1;
+    
+    while(left > right){
+        let addNum = numbers[left] + numbers[right]
+        if(addNum === target){
+            return [right + 1, left + 1]
+        }
+        if(addNum > target){
+            left--;
+        }else{
+            right++
+        }
+    }
+    
+    return []
+};
+
+
+// leetcode 167
+// s O(1)
+// t o(n)
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let l = 0;
+    let r = 0;
+
+    while(r < t.length){
+        if(s[l] === t[r]){
+            l++;
+        }
+        r++;
+    }
+
+    return l == s.length
+};
+
+
+// leetcode L= 392
+// s : O(1)
+// t O(n)
+
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let n = haystack.length;
+    let m = needle.length;
+
+    for(let i = 0; i <= n - m; i++){
+        let j = 0;
+        for(j = 0; j < m; j++){
+            if(haystack[i+j]!== needle[j]){
+                break;
+            }
+        }
+        if(j === m){
+            return i
+        }
+    }
+    return -1;
+};
+
+
+
+// leetcode  28
+// time O(n*m)
+// space O(1)
+
