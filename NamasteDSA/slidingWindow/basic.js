998a86ec30b2ad90a88749f1bd2c806051f10ee8
abhayhk29 <abhayhk29@gmail.com>
2025-07-18 21:50:28 +0530

    practice basic

--- NamasteDSA/slidingWindow/basic.js
+++ NamasteDSA/slidingWindow/basic.js
@@ -174,3 +174,299 @@ var strStr = function(haystack, needle) {
 // t O(n)
 // s O(m)
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    // let hashT = new Set();
+
+    // while(headB){
+    //     hashT.add(headB);
+    //     headB = headB.next;
+    // }
+
+    // while(headA){
+    //     if(hashT.has(headA)){
+    //         return headA;
+    //     }
+    //     headA = headA.next;
+    // }
+
+    // return null;
+
+    let n = 0;
+    let pn = headA;
+    while(pn){
+        n++;
+        pn = pn.next;
+    }
+
+    let m = 0;
+    let pb = headB;
+    while(pb){
+        m++;
+        pb = pb.next;
+    }
+
+    let diff = Math.abs(n - m);
+
+    if(n > m){
+        let temp = headA;
+        headA = headB;
+        headB = temp
+    }
+
+    for(let i = 0;  i < diff; i++){
+        headB = headB.next;
+    }
+
+    pn = headA;
+    pm = headB;
+
+    while(pn != pm){
+        pn = pn.next;
+        pm = pm.next;
+    }
+
+
+    return pn;
+
+    // let pa = headA;
+    // let pb = headB;
+
+    // while(pa != pb){
+    //     pa = pa === null ? headB : pa.next;
+    //     pb = pb === null ? headA : pb.next;
+    // }
+
+    // return pa;
+};
+
+// t O(m + n)
+// s O(1)
+
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    // let i = 0;
+    // let j = height.length - 1;
+    // let max = 0;
+    // while(i < j){
+    //     let temp = 0;
+    //     if(height[i] < height[j]){
+    //         temp = height[i] * ( j - i );
+    //         i++;
+    //     }else{
+    //         temp = height[j] * (j - i);
+    //         j--;
+    //     }
+    //         max = max > temp ? max : temp;
+    // }
+
+    // return max;
+
+    let i = 0; 
+    let j =  height.length - 1;
+    let max = 0;
+
+    while(i < j){
+        let tempMax = Math.min(height[i] , height[j])* (j - i);
+        max = Math.max(tempMax, max);
+        if(height[i] > height[j]){
+            --j
+        }else{
+            ++i;
+        }
+    }
+
+    return max;
+};
+
+
+// t :  O(n)
+//  s : O(1)
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(arr) {
+    // const res = [];
+    // nums.sort((a,b) => a - b);
+
+
+    // for(let i = 0; i < nums.length - 2; i++){
+        
+    //     let l = i + 1;
+    //     let r = nums.length - 1;
+
+    //     if (i > 0 && nums[i] === nums[i - 1]) continue
+
+    //     while(l < r){
+
+    //         let threeSum = nums[l] + nums[r];
+    //         if(threeSum == -nums[i]){
+    //             res.push([nums[i], nums[l], nums[r]]);
+    //             while(nums[l] === nums[l + 1]) l++;
+    //             while(nums[r] === nums[r - 1]) r--;
+    //             l++;
+    //             r--;
+    //         }
+    //         else if(threeSum > -nums[i]){
+    //             r--; 
+    //         }else if(threeSum < -nums[i]){
+    //             l++;
+    //         }
+    //     }
+    // }
+
+    // return res;
+    // let ans = [];
+    // arr.sort((a,b) => a - b);
+    // (n log n + 0(n^2))
+    // console.log(arr);
+//     let len = arr.length - 1;
+//     for (let i = 0; i < len; i++) {
+//         if(i > 0 && arr[i] === arr[i - 1]) continue;
+
+//         let j = i + 1;
+//         let k = len - 1;
+
+//         while( j < k){
+//             let sum  = arr[i] + arr[j] + arr[k];
+//             if(sum < 0){
+//                 j++;
+//             }else if(sum > 0){
+//                 k--;
+//             } else{
+//                 ans.push([arr[i], arr[j], arr[k]]);
+//                 j++;
+//                 // k--;
+//                 while(arr[j] == arr[j - 1] && j < k){
+//                     j++
+//                 }
+//                 // while( j < k && arr[j] == arr[j - 1]) j++;
+//                 // while(j < k && arr[k] == arr[k + 1]) k--;
+//             }
+//         }
+//     }
+
+//     return ans;
+    
+    // let res= [];
+    // arr.sort((a,b) => a - b);
+    
+    // for(let i = 0; i< arr.length; i++){
+    //     if(i > 0 && arr[i] === arr[i-1]){
+    //         continue;
+    //     }
+    //     let l = i + 1;
+    //     let r = arr.length - 1;
+        
+    //     while( l < r){
+    //         let sum = arr[i] + arr[l] + arr[r];
+    //         if(sum > 0){
+    //             r--;
+    //         }
+            
+    //         if(sum < 0){
+    //             l++;
+    //         }
+            
+    //         if(sum === 0){
+    //             res.push([arr[i] , arr[l] , arr[r]])
+    //             l++;
+    //             while(arr[l] === arr[l - 1] && l < r){
+    //                 l++;
+    //             }
+    //         }
+    //     }
+            
+    // }
+    // return res; 
+
+    arr.sort((a,b) => a - b);
+    let ans = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] != arr[i - 1]){
+         twoSum(arr,i,ans)
+        }
+    }
+
+
+
+    function twoSum(arr,x, ans){
+        let i = x + 1 ;
+        let j = arr.length - 1;
+
+        while(i < j){
+            let sum = arr[i] + arr[j] + arr[x];
+            if(sum === 0){
+                ans.push([arr[i], arr[j], arr[x]])
+                ++i;
+                --j;
+                while( i < j && arr[i] == arr[i - 1]) i++;
+            } else if(sum > 0){
+                --j
+            }else{
+                ++i;
+            }
+        }
+        return ans;
+    }
+
+    return ans;
+};
+
+// o(n^2)
+
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(arr) {
+    let n = arr.length;
+
+    let maxL = [];
+    maxL[0] = arr[0];
+    for(let i = 1; i < n; i++){
+        maxL[i] = Math.max(maxL[i - 1], arr[i]);
+    }
+
+    let maxR = [];
+    maxR[n - 1] = arr[n - 1];
+    for(let i = n - 2; i >= 0; i--){
+        maxR[i] = Math.max(maxR[i + 1], arr[i]);
+    }
+
+    let ans = 0;
+
+    for(let i = 0; i <n ; i++){
+        let waterTrapped = (Math.min(maxL[i], maxR[i]) - arr[i]);
+        ans += waterTrapped < 0 ? 0  : waterTrapped;
+    }
+
+    return ans;
+};
+
+// S O(n)
+// t O(n)
+
+
+
+
+
