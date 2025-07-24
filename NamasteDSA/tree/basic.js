f8e13fd220a6ca6c749a68004289963ee561e477
abhayhk29 <abhayhk29@gmail.com>
2025-07-24 09:50:05 +0530

    practice basic tree

--- NamasteDSA/tree/basic.js
+++ NamasteDSA/tree/basic.js
@@ -247,3 +247,86 @@ var postorderTraversal = function(root) {
 
 };
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    // let q = [root], ans = []
+    // while (q[0]) {
+    //     let qlen = q.length, row = []
+    //     for (let i = 0; i < qlen; i++) {
+    //         let curr = q.shift()
+    //         row.push(curr.val)
+    //         if (curr.left) q.push(curr.left)
+    //         if (curr.right) q.push(curr.right)
+    //     }
+    //     ans.push(row)            
+    // }
+    // return ans
+    // let res = [];
+
+    // if(!root){
+    //     return res;
+    // }
+
+    // let queue = [root];
+
+    // while(queue.length > 0){
+    //     let dummy = [];
+    //     let size = queue.length;
+    //     for (let i = 0; i < size; i++) {
+    //         const pop = queue.shift();
+    //         dummy.push(pop.val);
+    //         if(pop.left){
+    //             queue.push(pop.left)
+    //         }            
+
+    //         if(pop.right){
+    //             queue.push(pop.right);
+    //         }
+    //     }
+    //     res.push(dummy);
+    // }
+    // return res;
+
+    // let ans = [];
+    // if(!root){
+    //     return ans;
+    // }
+
+    // let queue = [root];
+
+    // while(queue.length){
+    //     let dummy = [];
+    //     let size = queue.length;
+    //     for(let i = 0; i < size; i++){
+    //         const fval = queue.shift();
+    //         dummy.push(fval.val);
+    //         if(fval.left) queue.push(fval.left)
+    //         if(fval.right) queue.push(fval.right)
+    //     }
+    //     ans.push(dummy);
+    // }
+    // return ans;
+    if(!root) return [];
+    let ans = [];
+
+    let traversal = function(curr,level){
+        if(!ans[level]) ans[level] = [];
+        ans[level].push(curr.val);
+        curr.left && traversal(curr.left, level + 1)
+        curr.right && traversal(curr.right, level + 1)
+    }
+    traversal(root,0)
+    return ans
+};
+
